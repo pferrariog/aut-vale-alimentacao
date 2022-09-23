04814ab95d6da39199554dbecf47d4f2eee27373
pferrariog <pdrferrari08@gmail.com>
2022-09-23 20:42:08 -0300

    feat: config input and refactor added

--- project/main.py
+++ project/main.py
@@ -1,13 +1,16 @@
+from datetime import date
+from os import getenv
+from time import sleep
+
+from dotenv import find_dotenv, load_dotenv
+from pandas import read_excel
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from time import sleep
-from datetime import date
-from os import getenv
-from dotenv import load_dotenv, find_dotenv
 
 
-def web_browser_connect(*options: str):
+def web_browser_connect(*options: str) -> webdriver.Chrome:
+    """Função p/ configurar o driver corretamente com a versão do browser"""
     chrome_options = webdriver.ChromeOptions()
 
     if options is not None:
@@ -21,6 +24,7 @@ def web_browser_connect(*options: str):
 
 
 def get_date():
+    """Função para retornar a data corretamente conforme o campo"""
     today = date.today()
     tomorrow = today.day + 1
     current_month = today.month
@@ -29,7 +33,8 @@ def get_date():
     return date_today
 
 
-def main_execution(chrome, user, password):
+def main_execution(chrome, user: str, password: str, df):
+    """Execução principal da automação"""
     chrome.get('https://wss.upbrasil.com/portalup/login.aspx')
     chrome.find_element(By.NAME, value='txtUsuarioEmpresa').send_keys(user)
     chrome.find_element(By.NAME, value='txtSenha').send_keys(password)
@@ -44,102 +49,13 @@ def main_execution(chrome, user, password):
     sleep(2)
 
     # setar valores por pessoa
-
-    # adelina
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl02$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_0').send_keys('6.50')
-    sleep(1)
-
-    # ademar
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl03$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_1').send_keys('548.76')
-    sleep(1)
-
-    # ageu
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl04$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_2').send_keys('548.76')
-    sleep(1)
-
-    # bruno
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl06$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_4').send_keys('548.76')
-    sleep(1)
-
-    # elias
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl10$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_8').send_keys('548.76')
-    sleep(1)
-
-    # filipe
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl14$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_12').send_keys('548.76')
-    sleep(1)
-
-    # jeorge
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl18$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_16').send_keys('548.76')
-    sleep(1)
-
-    # jó
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl19$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_17').send_keys('548.76')
-    sleep(1)
-
-    # josé
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl20$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_18').send_keys('548.76')
-    sleep(1)
-
-    # marcos
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl24$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_22').send_keys('548.76')
-    sleep(1)
-
-    # otávio
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl26$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_24').send_keys('548.76')
-    sleep(1)
-
-    # rafael
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl28$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_26').send_keys('548.76')
-    sleep(1)
-
-    # ronevon
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl32$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_30').send_keys('548.76')
-    sleep(1)
-
-    # sebastião
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl34$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_32').send_keys('548.76')
-    sleep(1)
-
-    # thiago
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl35$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_33').send_keys('548.76')
-    sleep(1)
-
-    # wanderson
-    chrome.find_element(By.NAME, value='ctl00$ContentPlaceHolder1$gvwCartoes$ctl36$chkUsuariosSelecionados').click()
-    sleep(1)
-    chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_gvwCartoes_txtValorCarga_34').send_keys('548.76')
-    sleep(4)
+    for index in df['ID']:
+        chrome.find_element(By.NAME,
+                            value=f'ctl00$ContentPlaceHolder1$gvwCartoes$ctl0{index}$chkUsuariosSelecionados').click()
+        sleep(1)
+        chrome.find_element(By.CSS_SELECTOR, value=f'#ContentPlaceHolder1_gvwCartoes_txtValorCarga_{index-2}').send_keys(
+            df['Valor recebido'][index])
+        sleep(1)
 
     # inclusão de usuários
     chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_btnIncluirUsuarios').click()
@@ -164,7 +80,7 @@ def main_execution(chrome, user, password):
     sleep(10)
 
     chrome.switch_to.alert.accept()
-    sleep(180)
+    sleep(120)
 
     chrome.find_element(By.CSS_SELECTOR, value='#ContentPlaceHolder1_btnBoleto').click()
     sleep(1)
@@ -179,7 +95,8 @@ if __name__ == '__main__':
     load_dotenv(find_dotenv())
     login = getenv("UP_LOGIN")
     senha = getenv("UP_SENHA")
+    df_func = read_excel(r'.\input\Dadosfunc.xlsx')
     order_date = get_date()
     browser_options = 'user-data-dir=Perfil'
     chrome_browser = web_browser_connect()
-    main_execution(chrome_browser, login, senha)
+    main_execution(chrome_browser, login, senha, df_func)
